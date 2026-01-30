fba027ea59fa58b2850d01d525e5a59bab3222db
Crackedyoda <nelsonakalia12@gmail.com>
2026-01-30 16:36:58 +0100

    FEAT: FIX

--- src/lib/bullredis.ts
+++ src/lib/bullredis.ts
@@ -3,5 +3,6 @@ import IORedis from "ioredis";
 export const bullRedis = new IORedis({
   host: process.env.REDIS_HOST || "127.0.0.1",
   port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
   maxRetriesPerRequest: null,
 });
